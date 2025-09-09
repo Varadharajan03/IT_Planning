28c253f3e629433e471e239daf4dcd3cc730f9f8
Sany A <sany.a.ihub@snsgroups.com>
2025-09-09 10:03:45 +0530

    boiler plate

---
+++ main.py
@@ -0,0 +1,8 @@
+from langchain.google_genai import GoogleGenAI
+from langchain import PromptTemplate, LLMChain
+from langchain.graphs import Graph, Node, Edge
+from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+#start of the code
